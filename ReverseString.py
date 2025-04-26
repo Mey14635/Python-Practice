23e8b993be2eda3ec28f8f8ece9654f86c7fde9e
Mey14635 <kasomediatrice1407@gmail.com>
2025-04-26 12:46:38 +0300

    commit 7

--- ReverseString.py
+++ ReverseString.py
@@ -1,5 +1,5 @@
-normal_string=input("enter a string: ")#receive the input from user
-opposite_string=""#it will store the new reverse words
-for letter in normal_string:#ie for each letter put it in front of the current opposite_string
-    opposite_string=letter+opposite_string#the new letter will push the existing forward as the loop is repeating
+normal_string=input("enter a string: ")  #receive the input from user
+opposite_string=""  #A empty string  will store the new reverse words
+for letter in normal_string: #ie for each letter put it in front of the current opposite_string
+    opposite_string=letter+opposite_string #the new letter will push the existing forward as the loop is repeating
 print(f"Reverse of string is : {opposite_string}")
