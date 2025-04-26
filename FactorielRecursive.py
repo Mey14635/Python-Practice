45c2212467ece05bef1d08a12ed2ce827c5bb2b2
Mey14635 <kasomediatrice1407@gmail.com>
2025-04-26 12:18:20 +0300

    commit 2

--- FactorielRecursive.py
+++ FactorielRecursive.py
@@ -7,5 +7,5 @@ else:
 #  the for loop will start from 1 till the number enter plus an extra number to include the no entered
     for i in range(1,number+1):
        factorial=factorial*i
-#if the no entered is 2 initially the fact is 1 ie 1*1,back to the loop then 1*2 will be store in the fact and so on
+# if the no entered is 2 initially the fact is 1 ie 1*1,back to the loop then 1*2 will be store in the fact and so on
 print(f"The factorial of {number} is : {factorial} ")
