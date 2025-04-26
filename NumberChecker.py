a206b6ace217d3f596ca2b004574c8a3eccdf90b
Mey14635 <kasomediatrice1407@gmail.com>
2025-04-26 12:39:38 +0300

    commit 6

---
+++ NumberChecker.py
@@ -0,0 +1,7 @@
+#we receive input from user
+number=int(input("Enter the number: "))
+if (number%2)==0:
+#cause the reminder of an even number is 0 and for ood is greater than 0
+    print(f"The number {number} is even")
+else:
+    print(f"The number {number} is odd")
